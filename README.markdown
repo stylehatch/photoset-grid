6ea016ee50103c12a707dd0b8e0f11624cb1d63d
Jonathan Moore <jonathan@stylehatch.co>
2013-02-18 17:47:37 -0800

    Initial commit and setting up the structure as a starting point for photosetGrid

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# photosetGrid
+
+A simple jQuery plugin to arrange images into a flexible grid, based on Tumblr's photoset feature.
+
+Originally the photosetGrid was designed for and used in our Style Hatch premium Tumblr themes, but now the plugin has been expanded for use on any site outside of Tumblr.
+
+_This is currently a work in progress while we transition our theme specific code to something that anyone can use._
---
+++ jquery.photosetGrid.js
@@ -0,0 +1,16 @@
+/**
+ * photosetGrid v1.0.0
+ * Arrange images into a flexible grid, based on Tumblr's photoset feature
+ *
+ * Originally designed for and used in Style Hatch premium Tumblr themes,
+ * but now the plugin has been expanded for use on any site outside of Tumblr.
+ *
+ * Copyright 2013 Jonathan Moore / Style Hatch
+ * http://stylehatch.co
+ */
+
+(function( window, $, undefined ){
+
+  'use strict';
+
+})( window, jQuery );
